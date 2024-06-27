3080a2a341215991f1d51fc319af666fe7512c39
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-06-27 16:34:39 +0600

    singIn with jwt

--- src/app/Module/Auth/Auth.service.ts
+++ src/app/Module/Auth/Auth.service.ts
@@ -1,7 +1,12 @@
+import httpStatus from "http-status";
 import AppError from "../../Error-Handle/AppError";
+import { validateLoginPassword } from "../../Re-Useable/BcryptValidatin";
 import { TUser } from "../User/User.interface";
 import { UserModel } from "../User/User.model";
 import { TSignIn } from "./Auth.interface";
+import { dynamicTokenGenerate } from "./Auth.utils";
+import dotenv from "dotenv";
+dotenv.config();
 
 const singUpDB = async (payload: Partial<TUser>) => {
   const result = await UserModel.create(payload);
@@ -14,6 +19,32 @@ const signInDB = async (payload: TSignIn) => {
   if (!user) {
     throw new AppError(404, "No Data Found");
   }
+  const checkPassword = await validateLoginPassword(password, user?.password);
+  if (!checkPassword) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Your Password dose not matched !!"
+    );
+  }
+
+  const jwtPayload = {
+    email: user?.email,
+    role: user?.role,
+  };
+
+  const accessToken = dynamicTokenGenerate(
+    jwtPayload,
+    process.env.SECRET_ACCESS_TOKEN as string,
+    process.env.SECRET_ACCESS_TOKEN_TIME as string
+  );
+  if (!accessToken) {
+    throw new AppError(httpStatus.CONFLICT, "Something Went Wrong !!");
+  }
+
+  return {
+    user,
+    accessToken,
+  };
 };
 
 export const authService = {
