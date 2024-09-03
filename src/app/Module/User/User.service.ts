35fc27fb0073b6b29985fa3f2cc8415ff865ca45
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-09-04 00:06:10 +0600

    user management fully done here

--- src/app/Module/User/User.service.ts
+++ src/app/Module/User/User.service.ts
@@ -8,7 +8,7 @@ const updateProfileDB = async (
   id: string,
   body: Partial<TUser>,
   tokenGetsId: string,
-  tokenGetsRole :string
+  tokenGetsRole: string
 ) => {
   const user = await UserModel.findById(id).select("+password");
   const tokenIdByUser = await UserModel.findById({ _id: tokenGetsId }).select(
@@ -18,19 +18,20 @@ const updateProfileDB = async (
   if (!user) {
     throw new AppError(httpStatus.BAD_REQUEST, "Data Not Found !!");
   }
-  if (user?.status === USER_STATUS.block) {
+  if (user?.status === USER_STATUS.block && tokenGetsRole === USER_ROLE.user) {
     throw new AppError(httpStatus.BAD_REQUEST, "This User Already Blocked !!");
   }
-  if (user?.isDelete) {
+  if (user?.isDelete && tokenGetsRole === USER_ROLE.user) {
     throw new AppError(httpStatus.BAD_REQUEST, "This User Already Delete !!");
   }
 
   if (
     (body?.role && tokenIdByUser?.role === USER_ROLE.user) ||
+    (body?.status && tokenIdByUser?.role === USER_ROLE.user) ||
     tokenIdByUser?.status === USER_STATUS?.block ||
     tokenIdByUser?.isDelete === true
   ) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Your Can not change Role !");
+    throw new AppError(httpStatus.BAD_REQUEST, "Your Can not change Role or status!");
   }
 
   if (body?.password) {
@@ -44,9 +45,11 @@ const updateProfileDB = async (
     throw new AppError(httpStatus.BAD_REQUEST, "Your Can not change email !");
   }
 
-
   if (tokenGetsRole === USER_ROLE.user && tokenGetsId.toString() !== id) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Your can't change Other user info");
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Your can't change Other user info"
+    );
   }
   const result = await UserModel.findByIdAndUpdate(
     id,
