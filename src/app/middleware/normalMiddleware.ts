5a71250fa6b94a5a521a07f93a0c0f88641a747b
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-09-13 16:45:24 +0600

    done

--- src/app/middleware/normalMiddleware.ts
+++ src/app/middleware/normalMiddleware.ts
@@ -1,14 +1,18 @@
 import express, { Application } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
 const normalMiddleware = (app: Application) => {
   app.use(
     cors({
-      origin: ["http://localhost:5173", "http://localhost:5174"],
+      origin: [
+        "https://mellow-crostata-06977e.netlify.app",
+        "http://localhost:5173",
+        "http://localhost:5174",
+      ],
       credentials: true,
-    }),
+    })
   );
   app.use(express.json());
   app.use(bodyParser.json());
